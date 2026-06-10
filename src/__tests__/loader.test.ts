ea1d2ba6eedf6b73698d352d7f4290e4389f3c8a
probablyduncan <duncanpetrie1@gmail.com>
2026-06-10 23:55:54 +0100

    init

---
+++ src/__tests__/loader.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'vitest'
+
+describe('astrena', () => {
+  it.todo('arenaLoader loads channel blocks into the data store')
+  it.todo('arenaLiveLoader.loadCollection returns channel blocks')
+  it.todo('arenaLiveLoader.loadEntry returns a single block by id')
+})
